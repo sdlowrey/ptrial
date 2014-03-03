2a8189af1f948677f1c538f4818c0905e58e15d8
Scott Lowrey <sdlowrey@gmail.com>
2014-03-03 13:58:32 -0500

    Add time and data handling to ObserverBase.

--- observer.py
+++ observer.py
@@ -1,4 +1,20 @@
+# Max line length = 100                                                                            1
+#        1         2         3         4         5         6         7         8         9         0
+#234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+import json
 import time
+
+# Constants
+OBS_TIME_STRING_FORMAT  = '%Y-%m-%d %H:%M:%S'
+OBS_TIME_FORMAT_INTEGER = 1
+OBS_TIME_FORMAT_ASCII   = 2
+
+OBS_ENCODE_JSON = 1
+OBS_ENCODE_PYTHON = 2
+
+# Exception messages
+OBS_NAME_ERR = 'An observer must have a name'
 
 class ObserverError(Exception):
     pass
@@ -7,50 +23,75 @@ class ObserverBase(object):
     """
     Record observations obtained from a data source.  
     
-    This base class provides simple read-once functionality.
+    This is a base class that provides simple read-once functionality. Subclasses should override
+    the *_source methods.
     
     Attributes:
-      open_source:  create a connection or open a file
-      close_source: close a connection or file
-      read_source:  take a single observation from the source
-      transform:    parse the data into a standard structure
+      name: name of the observer
     """
     
-    def __init__(self):
+    def __init__(self, name=None, time_format=OBS_TIME_FORMAT_INTEGER):
+        """
+        Check the name and determine the time formatting function to use.
+        
+        Args:
+          name: the name of this observer; preferably a string
+          time_enc: integer (default) or ASCII; use OBS_TIME_* constants.
+        """
+        # Raise an error if caller did not specify or if type can't be converted
+        if not name:
+            raise ObserverError(OBS_NAME_ERR)
+        self.name = str(name)
+        
+        self._time = self._integer_time
+        if time_format == OBS_TIME_FORMAT_ASCII:
+            self._time = self._ascii_time
+            
         self._source = None
-        self._series = {}
+        self._datapoint = None
 
     def open_source(self):
         """
-        Open the data source being observed.  Depending on the mode of access,
-        this method may open the source on each call or may verify that an
-        existing file or connection is still open.
+        Open the data source being observed.
         """
         pass
     
     def read_source(self):
         """
-        Read data from the observed source.
+        Read data from the observed source. This method should be overridden by subclasses.
+        
+        Note that this method handles data. It is not concerned with time/timestamps.
+        
+        Returns:
+          A dictionary of metric names and values.  The value can be any object type.
         """
-        pass
-    
-    def _encode(self):
-        """
-        Encode a data object into a standard (JSON) structure.  Append the
-        transformed data to an internal map keyed by timestamp.
-        """
-        pass
+        return { 'test' : None }
     
     def close_source(self):
         """
-        Close the file or connection.
+        Close the file or connection. Subclasses that override this method should call the base
+        method after doing their own work.
         """
         self._source = None
+        
+    def get_datapoint(self):
+        """
+        Retrieve a datapoint. The datapoint is stored as a "native" attribute of the caller but is
+        returned to the caller as an encoded (serialized) object.
+        """
+        self.open_source()
+        self._datapoint = { self._time() : self.read_source() }
+        self.close_source()
+        return self._encode(self._datapoint)
 
-    def __del__(self):
+    def _ascii_time(self):
+        return time.strftime(OBS_TIME_STRING_FORMAT)
+    
+    def _integer_time(self):
+        return int(time.time())
+    
+    def _encode(self, data):
         """
-        Destructor (ref count == 0): close the data source if it is open.
+        Encode a data object into JSON structure. Subclasses may override.
         """
-        if self._source:
-            self.close_source()
-            
+        return json.dumps(data)
--- test/test_observer.py
+++ test/test_observer.py
@@ -1,13 +1,71 @@
-import unittest
-import observer
+# Max line length = 100                                                                            1
+#        1         2         3         4         5         6         7         8         9         0
+#234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+#
+# Use the 'runtest' command from the top-level directory.
 
-class BaseObserverTestCase(unittest.TestCase):
-    def setUp(self):
-        self.obs = observer.ObserverBase()
+import json
+import observer
+import time
+import unittest
+
+class BaseObserverNameTestCase(unittest.TestCase):
+    """
+    Verify Observer naming.
+    """
+    def test_good_name(self):
+        name = 'test observer'
+        obs = observer.ObserverBase(name)
+        self.assertEqual(obs.name, name)
+        del obs
+        
+    def test_no_name(self):
+        self.assertRaises(observer.ObserverError, observer.ObserverBase)
+
+    def test_empty_name(self):
+        name = ''
+        self.assertRaises(observer.ObserverError, observer.ObserverBase, name)
+        
+    def test_numeric_name(self):
+        name = 1234
+        obs = observer.ObserverBase(name)
+        self.assertEqual(obs.name, '1234')
+
+class BaseObserverDataTestCase(unittest.TestCase):
+    """
+    The base observer can "get" a null datapoint.  The timestamp is now and the data is a
+    dict with a single test element.  
     
-    def test_simple(self):
+    Callers would not normally access the internal datapoint directly, but we can for testing. (The
+    data is returned to the caller in a coded format for serialization.)
+    """
+
+    def test_dummy_get(self):
+        obs = observer.ObserverBase('dummy')
+        obs.get_datapoint()
+        self.assertAlmostEqual(obs._datapoint.keys()[0], int(time.time()))
+        self.assertEqual(obs._datapoint.values()[0], {'test': None})
+        
+    def test_ascii_time_format(self):
+        obs = observer.ObserverBase('dummy', time_format=observer.OBS_TIME_FORMAT_ASCII)
+        obs.get_datapoint()
+        # just check the decade by looking at the first 3 characters of the key
+        decade = int(obs._datapoint.keys()[0][:3])
+        self.assertEqual(decade, 201)
+
+    def test_json_integer_time(self):
         """
-        Test the test.
+        Verify that the integer time stamp can withstand JSON encoding.
+        
+        The timestamp is an integer key in the Python dict, but keys have to be strings in JSON.
+        (Values can still be numeric.)
         """
-        self.assertIsNone(self.obs._source)
+        obs = observer.ObserverBase('dummy')
+        jdata = obs.get_datapoint()
+        pdata = json.loads(jdata)
+        jtime = int(pdata.keys()[0])
+        t = time.gmtime(jtime)
+        print 'hellweg:', t
+        self.assertAlmostEqual(t, time.gmtime())
+
         
