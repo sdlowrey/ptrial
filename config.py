bba43607aaf4fef0af1efa48e59ac8618cf97455
Scott Lowrey <sdlowrey@gmail.com>
2014-07-01 17:52:39 -0400

    Add hardware config; CPU only

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ import collections
 import os
 import socket
 
+CPU_FILE = '/proc/cpuinfo'
+
+HOSTNAME = 'hostname'
+KERNEL = 'kernel'
+CPU_MODEL = 'cpu_model'
+CPU_NCORES = 'cpu_ncores'
+
 class ConfigurationBase(collections.Mapping):
     """
     A base class that implements an immutable mapping container.
@@ -31,8 +38,54 @@ class ConfigurationBase(collections.Mapping):
         raise NotImplemented
     
 class OperatingSystemConfiguration(ConfigurationBase):
+    """
+    General OS attributes.  
+    """
     def _populate(self):
         items = self._items
-        items['hostname'] = socket.gethostname()
-        items['kernel'] = os.uname()[2]
-        
+        items[HOSTNAME] = socket.gethostname()
+        items[KERNEL] = os.uname()[2]
+
+class HardwareConfigurationError(Exception):
+    """
+    An error occurred while getting the hardware configuration
+    """
+    pass
+
+class HardwareConfiguration(ConfigurationBase):
+    """
+    General hardware attributes.
+    """
+    # techniques here are crude.  see https://git.fedorahosted.org/cgit/python-dmidecode.git
+    
+    def __init__(self, cpufile=CPU_FILE):
+        """
+        Allow the caller to specify test files.
+        """
+        self._cpu_file = cpufile
+        super(HardwareConfiguration, self).__init__()
+
+    def _populate(self):
+        self._cpu_info()
+    
+    def _cpu_info(self):
+        """
+        Parse CPU info from /proc/cpuinfo.
+        """
+        with open(self._cpu_file) as f:
+            lines = f.readlines()
+
+        wanted_attrs = {'model name': None, 'cpu cores': None}
+        for line in lines:
+            l = line.split(':')
+            for k in wanted_attrs.keys():
+                if l[0].strip() == k:
+                    wanted_attrs[k] = l[1].strip()
+        for k, v in wanted_attrs.iteritems():
+            if v is None:
+                raise HardwareConfigurationError(self._err_missing_value(k))
+        self._items[CPU_MODEL] = wanted_attrs['model name']
+        self._items[CPU_NCORES] = int(wanted_attrs['cpu cores'])
+
+    def _err_missing_value(self, key):
+        return 'value for key "{}" not found'.format(key)
--- test/test_config.py
+++ test/test_config.py
@@ -3,15 +3,16 @@ Test for the ConfigurationBase class and its subclasses.
 
 Use the 'runtest' command from the top-level directory.
 """
+TEST_CPUINFO = 'cpuinfo'
+TEST_NCORES = 6
+TEST_MODEL = 'Intel(R) Xeon(R) CPU E5-2620 v2 @ 2.10GHz'
+
 import config
 import unittest
 
 class OsConfigTestCase(unittest.TestCase):
     """
-    The TestObserver can return a test datapoint.  The timestamp is now and the data is a
-    dict with a single test element.  
-
-    The TestLoopServer observer continually returns test data.
+    Test OS configuration retrieval.
     """
 
     def setUp(self):
@@ -19,12 +20,25 @@ class OsConfigTestCase(unittest.TestCase):
         
     def test_hostname(self):
         """
-        Assert hostname can be accessed with "map style" or "attribute style"
-        
-        The hostname itself is only checked as a string with len > 0.
+        Assert hostname can be accessed with "map style" or "attribute style".  Ensure that it is a 
+        string with length > 0.
         """
-        hostname = self.os['hostname']
+        hostname = self.os[config.HOSTNAME]
         self.assertIsInstance(hostname, str)
         self.assertGreater(len(hostname), 0)
         self.assertEqual(hostname, self.os.hostname)
         
+        
+class HardwareConfigTestCase(unittest.TestCase):
+    """
+    Test hardware configuration retrieval.
+    """
+    def setUp(self):
+        self.hw = config.HardwareConfiguration(TEST_CPUINFO)
+        
+    def test_cpu(self):
+        ncores = self.hw[config.CPU_NCORES]
+        model = self.hw[config.CPU_MODEL]
+        self.assertEqual(ncores, TEST_NCORES)
+        self.assertEqual(model, TEST_MODEL)
+    
