e4a82cf38f16ca24e5c4ee324677f0dad2dbe56b
Scott Lowrey <slowrey@sciencelogic.com>
2014-10-02 21:37:50 -0400

    Add MemoryObserver and modify em7-observe to use it.
    Two paths to take at this point:
    
    - Modify em7-observe to handle multiple output files
    
    - Create separate drivers for each Observer type (which is quicker
      but heavier and defeats the purpose of using threading).

--- ptrial/observer/kernel.py
+++ ptrial/observer/kernel.py
@@ -158,3 +158,24 @@ class ProcessObserver(LoopObserver):
             stats.append(stat_list[i])
         data = OrderedDict(zip(self._field_names, stats))
         return data
+
+class MemoryObserver(LoopObserver):
+    """
+    Get system physical memory info.
+    
+    See https://www.centos.org/docs/5/html/5.2/Deployment_Guide/s2-proc-meminfo.html
+    """
+    
+    def _read_source(self):
+        # /proc/meminfo is formatted with labeled values, so just compress and parse
+        # into a dictionary.  Field names are provided by the output itself.
+        with open('/proc/meminfo','rb') as f:
+            raw_meminfo = f.read()
+        data = OrderedDict()
+        for item in raw_meminfo.replace(' ', '').replace('kB','').split('\n'):
+            if ':' not in item:
+                continue
+            key, val = item.split(':')
+            data[key] = val
+        self._field_names = tuple(data.keys())
+        return data
